34aeba44681f594996b1652fa4fcebb381cc36dc
RG <rula018@gmail.com>
2016-11-07 20:27:30 -0700

    Created constructor and attacker function

--- src/SG/MyAgent.java
+++ src/SG/MyAgent.java
@@ -1,6 +1,17 @@
 package SG;
 
 public class MyAgent extends Player{
+    private final String newName = "MyAgent";
+    public MyAgent(){
+        super();
+        playerName = newName;
+    }
 
+    protected int attackTarget(GameModel g, double[] coverage){
+        int[] UAU = g.getPayoffs()[1];
+        int[] UAC = g.getPayoffs()[0];
+
+        return (int) 5;
+    }
 }
 
